f589ad705537c7969b1ec2df9e055d73fb02e42b
aaron mitchell <aaronmitchell9902@gmail.com>
2023-03-10 14:08:04 -0500

    gut auth

--- StackNavigator.tsx
+++ StackNavigator.tsx
@@ -1,20 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import ChatScreen from './screens/ChatScreen';
-import LoginScreen from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
+
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-      <Stack.Screen name='Chat' component={ChatScreen} />
-      <Stack.Screen name='Login' component={LoginScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          {/* <Stack.Screen name="Chat" component={ChatScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} /> */}
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default StackNavigator
+export default StackNavigator;
